09439796a67aca824b92220614d8f8839de0fe06
zza20123 <phitthaya_0908@hotmail.com>
2017-07-12 03:12:02 +0700

    Update sms.php

--- sms.php
+++ sms.php
@@ -1,1 +1,22 @@
-# sms
+<?php
+
+// กำหนด UID ของท่าน
+define('UID', '135198');
+
+// กำหนด API Token ของท่าน
+define('TOKEN', '814289b345d75e82192b2abb2b07215c');
+
+$postfields = 'uid=' . UID . '&token=' . TOKEN . '&msisdn=0618354266&message=' . rawurlencode('คิดถึงคุณที่สุด');
+
+$curl = curl_init('https://www.tmtopup.com/sendsms_api.php');
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+curl_setopt($curl, CURLOPT_HEADER, FALSE);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE);
+curl_setopt($curl, CURLOPT_POST, TRUE);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $postfields);
+$curl_content = curl_exec($curl);
+curl_close($curl);
+?>
